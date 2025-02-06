c62c18904f5522e450299c5b913f9e986e453a4a
Hamdan Mohamed <hamdan.mohamed@vdabcampus.be>
2025-02-06 11:25:01 +0100

    Oefening 3.10 af. Bladzijde 41.

---
+++ Blazor/BlazorServer/Components/Pages/Dobbelen.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Components.Pages;
+
+public partial class Dobbelen
+{
+    private int currentScore = 0;
+    private List<int> stenen = new List<int>();
+    [Parameter]
+    public int Aantal { get; set; }
+    private void GooiStenen()
+    {
+        currentScore = 0;
+        stenen.Clear();
+        Random random = new Random();
+        for (var index = 0; index < Aantal; index++)
+        {
+            var ogen = random.Next(1, 7);
+            stenen.Add(ogen);
+            currentScore += ogen;
+        }
+    }
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        if (Aantal == 0) Aantal = 2;
+    }
+}
---
+++ Blazor/BlazorServer/Components/Pages/EenentwintigBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Components.Pages;
+
+public class EenentwintigBase : ComponentBase
+{
+    protected int currentScore = 0;
+    protected List<string> kaarten = new List<string>();
+    private static readonly string[] Soorten = { "H", "D", "C", "S" };
+    private static readonly string[] Beelden = { "7", "8", "9", "10",
+                                                 "J", "Q", "K", "A" };
+    private static readonly byte[] Waarden = { 7, 8, 9, 10, 1, 2, 3, 11 };
+
+    protected CopyrightFooter? cfdynamic;
+    private string GetRandomCard()
+    {
+        Random random = new Random();
+        var soort = Soorten[random.Next(0, 4)];
+        var kaart = random.Next(0, 8);
+        var beeld = Beelden[kaart];
+        var waarde = Waarden[kaart];
+        currentScore += waarde;
+        return $"{beeld}{soort}";
+    }
+    protected Dictionary<string, object> footerAttributes =
+     new Dictionary<string, object>() {
+         {
+            "auteur", "Hamdan"
+         },
+         {
+            "cursus", "Blazor"
+         },
+         {
+            "jaartal", 2025
+         },
+         {
+            "doelgroep", ".NET ontwikkeling met C#"
+         },
+     };
+    protected void Verander() => footerAttributes["auteur"] = "Alina";
+
+    protected string? cursus;
+    protected string? info;
+    protected void InfoOphalen()
+    {
+        cursus = cfdynamic?.Cursus;
+        info = cfdynamic?.AlleInfo();
+    }
+
+    protected void TrekKaart()
+    {
+        kaarten.Add(GetRandomCard());
+        if (currentScore > 21)
+        {
+            throw new InvalidOperationException("Je bent verbrand!");
+        }
+    }
+}
