9213536cc0dc50838fd6cfb72c911c2aa2392588
Hamdan Mohamed <hamdan.mohamed@vdabcampus.be>
2025-02-14 13:18:19 +0100

    Hoofdstuk 13 af.

---
+++ Blazor/BlazorServer/Data/Data.cs
@@ -0,0 +1,18 @@
+﻿using BlazorServer.Models;
+using Microsoft.JSInterop;
+
+namespace BlazorServer.Data;
+
+public class Data
+{
+    private static List<Persoon> personen = new List<Persoon> {
+ new Persoon { Id = 1, Voornaam = "Jesse", Familienaam = "James" },
+ new Persoon { Id = 2, Voornaam = "Jane", Familienaam = "Calamity" }
+ };
+    [JSInvokable]
+    public static List<Persoon> GetPersonen()
+    {
+        return personen;
+    }
+
+}
---
+++ Blazor/BlazorServer/Models/Groeter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorServer.Models;
+
+public class Groeter
+{
+    public Groeter(string naam)
+    {
+        Naam = naam;
+    }
+    public string Naam { get; set; }
+    [JSInvokable]
+    public string Begroet() => $"Hallo, {Naam}!";
+}
